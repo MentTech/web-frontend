0aabb487aaf15b105dce3e5135377d592b9c90ce
lequocdatfit <18120305@student.hcmus.edu.vn>
2022-06-09 18:26:39 +0700

    Change login page

--- pages/authenticate/login.tsx
+++ pages/authenticate/login.tsx
@@ -1,22 +1,25 @@
+import * as React from 'react'
+import Link from 'next/link'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { loginPayload } from '@models/auth'
 import { signIn, useSession } from 'next-auth/react'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { toast } from 'react-toastify'
 import * as yup from 'yup'
 
+export interface LoginProps {}
+
 const schema = yup
   .object({
-    email: yup.string().email().required(),
-    password: yup.string().max(32).min(6).required(),
+    email: yup.string().email().required('Email không được bỏ trống!'),
+    password: yup.string().max(32).min(6).required('Mật khẩu không được bỏ trống!'),
     // isMentor: yup.boolean().default(false).required(),
   })
   .required()
 
-export default function Login() {
+export default function Login(props: LoginProps) {
   const { data: session, status } = useSession()
   const [loading, setLoading] = useState(true)
   const [isMentor, setIsMentor] = useState(false)
@@ -39,7 +42,7 @@ export default function Login() {
         redirect: false,
       }).then((res: any) => {
         if (res?.ok) {
-          router.push('/mentor/profile')
+          router.push('/mentor')
         } else {
           console.log(res?.err)
           toast.error('Credentials do not match!', { type: 'error' })
@@ -60,25 +63,45 @@ export default function Login() {
       })
     }
   }
-
-  // if (loading) {
-  //   return <LinearIndeterminate />
-  // }
-
   return (
-    <>
-      <div className="flex justify-center min-h-screen items-center bg-gradient-to-tl from-green-400 to-indigo-900">
-        <div className="flex flex-col w-full max-w-md px-4 py-8 bg-gray-100 rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-          <div className="self-center mb-8 text-xl font-bold text-gray-600 sm:text-3xl dark:text-white">
-            ĐĂNG NHẬP
+    <div className="2xl:container h-screen m-auto">
+      <div hidden className="fixed inset-0 w-6/12 lg:block">
+        <div className="w-full h-full loginbackground text-white flex items-center justify-center">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold mb-2 opacity-100">Xin chào!</h2>
+            <div className="border-2 w-10 border-white inline-block mb-2"></div>
+            <p className="mb-4">Điền đầy đủ thông tin và bắt đầu hành trình cùng chúng tôi.</p>
+            <Link href="/authenticate/register">
+              <a className="inline-block cursor-pointer border-2 border-white rounded-full px-12 py-2 font-semibold hover:bg-white hover:text-black duration-150">
+                Đăng ký
+              </a>
+            </Link>
           </div>
-          <div className="df aic jcc w100">
+        </div>
+      </div>
+      <div
+        hidden
+        role="hidden"
+        className="fixed w-6/12 ml-auto bg-white bg-opacity-70 lg:block"
+      ></div>
+      <div className="relative h-full ml-auto lg:w-6/12">
+        <div className="m-auto py-12 px-6 sm:p-6 xl:w-10/12">
+          <div className="space-y-2">
+            <Link href="/">
+              <a className="inline-block">
+                <img src="/static/logo2.png" className="w-36" alt="MentTech" />
+              </a>
+            </Link>
+            <h3 className="font-medium text-lg text-gray-600 mt-8">Loại tài khoản</h3>
+          </div>
+
+          <div className="mt-4">
             <input name="accountType" id="mentee" type="radio" hidden defaultChecked />
             <label
               onClick={() => setIsMentor(false)}
               htmlFor="mentee"
-              className={`btn btn-outline btn-success hover:text-white ${
-                isMentor ? '' : 'bg-success text-white'
+              className={`btn btn-outline border-sky-600 hover:text-white hover:bg-sky-600 ${
+                isMentor ? '' : 'bg-sky-600 text-white'
               }`}
             >
               MENTEE
@@ -87,143 +110,148 @@ export default function Login() {
             <label
               onClick={() => setIsMentor(true)}
               htmlFor="mentor"
-              className={`btn btn-outline btn-success ml-3 hover:text-white ${
-                isMentor ? 'bg-success text-white' : ''
+              className={`btn btn-outline border-sky-600 ml-3 hover:text-white hover:bg-sky-600 ${
+                isMentor ? 'bg-sky-600 text-white' : ''
               }`}
             >
               MENTOR
             </label>
           </div>
 
-          <div className="mt-4">
-            <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-              <input name="csrfToken" type="hidden" />
+          <form autoComplete="off" onSubmit={handleSubmit(onSubmit)} className="space-y-6 py-6">
+            <div>
+              <input
+                type="email"
+                {...register('email')}
+                placeholder="Email"
+                className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-lg placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+              />
+              <p className="text-red-600">{errors.email?.message}</p>
+            </div>
 
-              <div className="flex flex-col mb-2">
-                <div className="flex relative ">
-                  <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
-                    <svg
-                      width="15"
-                      height="15"
-                      fill="currentColor"
-                      viewBox="0 0 1792 1792"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"></path>
-                    </svg>
-                  </span>
-                  <input
-                    type="text"
-                    {...register('email')}
-                    className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder="Your email"
-                  />
-                </div>
-                <p className="text-red-600">{errors.email?.message}</p>
-              </div>
-              <div className="flex flex-col mb-6">
-                <div className="flex relative ">
-                  <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
-                    <svg
-                      width="15"
-                      height="15"
-                      fill="currentColor"
-                      viewBox="0 0 1792 1792"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"></path>
-                    </svg>
-                  </span>
-                  <input
-                    type="password"
-                    {...register('password')}
-                    className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder="Your password"
-                  />
-                </div>
-              </div>
-              {/* <div className="flex flex-col mb-6">
-                <div className="flex relative ">
-                  <div className="form-control">
-                    <label className="cursor-pointer label">
-                      <input
-                        type="checkbox"
-                        className="checkbox checkbox-accent"
-                        {...register('isMentor')}
-                      />
-                      <span className="label-text ml-2">Bạn là mentor?</span>
-                    </label>
-                  </div>
-                </div>
-              </div> */}
-
-              {/* <div className="flex items-center mb-6 -mt-4">
-                <div className="flex ml-auto">
-                  <Link href="/auth/forgot-password">
-                    <a className="inline-flex text-xs font-thin text-gray-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white">
-                      Forgot Your Password?
-                    </a>
-                  </Link>
-                </div>
-              </div> */}
+            <div className="flex flex-col items-end">
+              <input
+                type="password"
+                {...register('password')}
+                placeholder="Mật khẩu"
+                className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-lg placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+              />
               <p className="text-red-600">{errors.password?.message}</p>
-
-              <button className="cursor-pointer py-2 px-4 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                Login
+              {/* <button type="reset" className="w-max p-3 -mr-3">
+                <span className="text-sm tracking-wide text-sky-600">Quên mật khẩu ?</span>
+              </button> */}
+            </div>
+            <div>
+              <button className="m-0 w-full px-6 py-3 rounded-lg bg-sky-800 transition hover:bg-sky-900 focus:bg-sky-900 active:bg-sky-900">
+                <span className="font-semibold text-white text-lg">Đăng nhập</span>
               </button>
-            </form>
-          </div>
-          {!isMentor && (
-            <>
-              <div className="flex gap-4 item-center mt-4">
-                <button
-                  onClick={() => signIn('facebook', { callbackUrl: '/find' })}
-                  type="button"
-                  className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                >
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="mr-2"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z"></path>
-                  </svg>
-                  Facebook
-                </button>
+              <button type="reset" className="w-max p-3 -ml-3">
+                <span className="text-sm tracking-wide text-sky-600">Đăng ký tài khoản</span>
+              </button>
+            </div>
 
-                <button
-                  onClick={() => signIn('google', { callbackUrl: '/find' })}
-                  type="button"
-                  className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                >
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="mr-2"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
-                  </svg>
-                  Google
-                </button>
-              </div>
-              <div className="flex items-center justify-center mt-6">
-                <div className="inline-flex  items-center text-xs text-center text-gray-900 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
-                  <span className="ml-2">Bạn chưa có tài khoản?</span>
-                  <Link href="/authenticate/register">
-                    <a className="px-1 text-blue-500 cursor-pointer">Đăng ký</a>
-                  </Link>
+            {!isMentor && (
+              <>
+                <div role="hidden" className="mt-12 border-t">
+                  <span className="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white">
+                    Hoặc
+                  </span>
                 </div>
-              </div>
-            </>
-          )}
+
+                <div className="mt-12 grid gap-6 sm:grid-cols-2">
+                  <button
+                    className="h-12 px-6 border border-blue-100 rounded-lg bg-blue-50 hover:bg-blue-100 focus:bg-blue-100 active:bg-blue-200"
+                    onClick={() => signIn('google', { callbackUrl: '/find' })}
+                  >
+                    <div className="flex items-center space-x-4 justify-center">
+                      <div className="google-icon-wrapper">
+                        <img
+                          className="google-icon"
+                          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                        />
+                      </div>
+                      <span className="block w-max font-medium tracking-wide text-sm text-blue-700">
+                        Tiếp tục với Google
+                      </span>
+                    </div>
+                  </button>
+                  <button
+                    className="h-12 px-6 rounded-lg btn-outline border text-blue-700 hover:text-white border-blue-500 transition hover:bg-blue-600 active:bg-blue-600 focus:bg-blue-700"
+                    onClick={() => signIn('facebook', { callbackUrl: '/find' })}
+                  >
+                    <div className="flex space-x-4 items-center justify-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        data-name="Ebene 1"
+                        viewBox="0 0 1024 1024"
+                        width={24}
+                        height={24}
+                      >
+                        <path
+                          fill="#1877f2"
+                          d="M1024,512C1024,229.23016,794.76978,0,512,0S0,229.23016,0,512c0,255.554,187.231,467.37012,432,505.77777V660H302V512H432V399.2C432,270.87982,508.43854,200,625.38922,200,681.40765,200,740,210,740,210V336H675.43713C611.83508,336,592,375.46667,592,415.95728V512H734L711.3,660H592v357.77777C836.769,979.37012,1024,767.554,1024,512Z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M711.3,660,734,512H592V415.95728C592,375.46667,611.83508,336,675.43713,336H740V210s-58.59235-10-114.61078-10C508.43854,200,432,270.87982,432,399.2V512H302V660H432v357.77777a517.39619,517.39619,0,0,0,160,0V660Z"
+                        />
+                      </svg>
+                      <span className="block w-max font-medium tracking-wide text-sm">
+                        Tiếp tục với Facebook
+                      </span>
+                    </div>
+                  </button>
+                </div>
+              </>
+            )}
+          </form>
+
+          <div className="border-t text-gray-500 pt-12">
+            <div className="text-center space-x-4">
+              <span>&copy; MentTech</span>
+              <a href="#" className="text-sm hover:text-sky-900">
+                Contact
+              </a>
+              <a href="#" className="text-sm hover:text-sky-900">
+                Privacy & Terms
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
+}
+
+{
+  /* <h2 className="mb-8 text-2xl text-cyan-900 font-bold">Sign in to your account</h2>
+                <form action="" className="space-y-8 py-6">
+                  <div>
+                    <input
+                      type="email"
+                      placeholder="Your Email"
+                      className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-lg placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+                    />
+                  </div>
+
+                  <div className="flex flex-col items-end">
+                    <input
+                      type="password"
+                      placeholder="What's the secret word ?"
+                      className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-lg placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+                    />
+                    <button type="reset" className="w-max p-3 -mr-3">
+                      <span className="text-sm tracking-wide text-sky-600">Forgot password ?</span>
+                    </button>
+                  </div>
+
+                  <div>
+                    <button className="w-full px-6 py-3 rounded-lg bg-sky-500 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800">
+                      <span className="font-semibold text-white text-lg">Login</span>
+                    </button>
+                    <button type="reset" className="w-max p-3 -ml-3">
+                      <span className="text-sm tracking-wide text-sky-600">Create new account</span>
+                    </button>
+                  </div>
+                </form> */
 }
